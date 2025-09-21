24d96a50644adb3ee8e238b835ba52d216559e90
rgermain <contact@germainremi.fr>
2025-09-21 21:41:17 +0200

    clean repo

---
+++ dotfiles_manager/__init__.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+from dotfiles_manager.commands.copy import copy_command
+from dotfiles_manager.commands.extra import backup_command, refresh_command
+from dotfiles_manager.commands.init import (
+    init_command,
+    init_copy_command,
+    init_link_command,
+)
+from dotfiles_manager.commands.runner import runner
+from dotfiles_manager.commands.symlink import link_command, unlink_command
+from dotfiles_manager.utils.style import style
+
+type = argparse.FileType()
+
+
+def main():
+    parser = argparse.ArgumentParser("dotfiles manager")
+    parser.add_argument(
+        "-y", action="store_true", default=False, help="assume yes"
+    )
+    parser.add_argument(
+        "-n", action="store_true", default=False, help="assume no"
+    )
+    parser.add_argument(
+        "-c",
+        "--no-color",
+        action="store_true",
+        default=False,
+        help="not color output",
+    )
+    sub = parser.add_subparsers(title="commands", dest="command", required=True)
+
+    init = sub.add_parser("init", help="symlink/copy all dotfiles")
+    init.add_argument(
+        "--only",
+        choices=["home", "system"],
+        help="link only specified element",
+        default=None,
+        dest="only",
+    )
+    init.add_argument("--config", help="config path", default=[], nargs="+")
+
+    init_link = sub.add_parser("init-link", help="symlink all dotfiles")
+    init_link.add_argument(
+        "--only",
+        choices=["home", "system"],
+        help="link only specified element",
+        default=None,
+        dest="only",
+    )
+
+    init_copy = sub.add_parser("init-copy", help="copy all dotfiles")
+    init_copy.add_argument(
+        "--only",
+        choices=["home", "system"],
+        help="link only specified element",
+        default=None,
+        dest="only",
+    )
+    init_copy.add_argument(
+        "--config", help="config path", default=[], nargs="+"
+    )
+
+    link = sub.add_parser("link", aliases=("ln",), help="link dotfiles file(s)")
+    link.add_argument("src", help="file/dir to add link", nargs="+")
+
+    link = sub.add_parser(
+        "unlink", aliases=("ul",), help="unlink dotfiles file(s)"
+    )
+    link.add_argument("src", help="file/dir to remove link", nargs="+")
+    link.add_argument(
+        "--no-remove",
+        action="store_true",
+        default=False,
+        help="no remove dotfiles linked",
+    )
+
+    copy = sub.add_parser("copy", aliases=("cp",), help="copy dotfiles file(s)")
+    copy.add_argument("src", help="file/dir to copy", nargs="+")
+
+    sub.add_parser(
+        "backup",
+        aliases=("bc",),
+        help="backup dotfiles, show diff, and push to git",
+    )
+    sub.add_parser(
+        "refresh",
+        aliases=("up", "update", "upgrade"),
+        help="run a refreshs scripts (update packages ..ect)",
+    )
+
+    flags = parser.parse_args()
+
+    style.config(flags)
+
+    try:
+        if flags.command == "init":
+            runner(init_command(flags), flags)
+        elif flags.command == "init-link":
+            runner(init_link_command(flags), flags)
+        elif flags.command == "init-copy":
+            runner(init_copy_command(flags), flags)
+        elif flags.command in ("link", "ln"):
+            runner(link_command(flags.src, flags), flags)
+        elif flags.command in ("unlink", "ul"):
+            runner(unlink_command(flags.src, flags), flags)
+        elif flags.command in ("copy", "cp"):
+            runner(copy_command(flags.src, flags), flags)
+        elif flags.command in ("backup", "bc"):
+            backup_command(flags)
+        elif flags.command in ("refresh", "up", "upgrade", "update"):
+            refresh_command(flags)
+        else:
+            sys.exit(f"invalid command {flags.command!r}")
+    except KeyboardInterrupt:
+        exit(1)
+
+
+def cli():
+    exit(main(sys.argv[1:]))
+
+
+if __name__ == "__main__":
+    cli()
--- dotfiles_manager/commands/extra.py
+++ dotfiles_manager/commands/extra.py
@@ -1,13 +1,21 @@
 import subprocess
+import sys
 
 from dotfiles_manager.utils.config import BASE_DOTFILE
 
 
 def refresh_command(flags):
+    script = BASE_DOTFILE / "refresh.sh"
+    if not script.exists():
+        sys.exit("refresh.sh script don't exists")
     subprocess.run(["bash", "refresh.sh"], cwd=BASE_DOTFILE)
 
 
 def backup_command(flags):
+    script = BASE_DOTFILE / "backup.sh"
+    if not script.exists():
+        sys.exit("backup.sh script don't exists")
+
     cmds = ["bash", "backup.sh"]
     if flags.n:
         cmds.append("-n")
